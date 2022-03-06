58686afd775bbed2b0b21e5e0630233b2b1c102a
AsafO7 <57495007+AsafO7@users.noreply.github.com>
2022-03-06 13:34:11 +0200

    Add files via upload

--- src/components/main-page/Home.tsx
+++ src/components/main-page/Home.tsx
@@ -14,50 +14,56 @@ import { Link } from "react-router-dom";
 export const Home = () => {
     return (
         <main className="main-container">
-            <section className="skills">
-                <div className="about">
-                    <h3 className="about-header">My name is Asaf Ovadya</h3>
-                    <p className="about-me">C.S student of The
-                        Open University of Israel.
-                    </p>
-                    <h4>I'm looking for my first job as a Front-end developer.</h4>
-                    <Link to="/contact"><button className="contact">Contact me</button></Link>
-                </div>
-                <div className="my-image">
-                    {/* <h2 className="skill-header">Skills</h2>
-                    <div className="icons">
-                        <img className="icon" src="https://img.icons8.com/color/144/000000/html-5--v2.png" alt="html"
-                        loading="lazy"/>
-                        <img className="icon" src="https://img.icons8.com/color/144/000000/css3.png" alt="css"
-                        loading="lazy"/>
-                        <img className="icon" src="https://img.icons8.com/color/144/000000/javascript--v1.png" alt="js"
-                        loading="lazy"/>
-                        <img className="icon" src="https://img.icons8.com/color/144/000000/react-native.png" alt="react"
-                        loading="lazy"/>
-                    </div> */}
-                    Future picture here
-                </div>
-            </section>
-            <section className="projects">
-                <div className="project">
-                    <Casino fontSize="large" color="action"/>
-                    <h3>Mini games project</h3>
-                    <h4>Vanilla Javascript</h4>
-                    <a href="https://small-mini-games-project.netlify.app/" className="pbtn">Check out</a>
-                </div>
-                <div className="project">
-                    <RestaurantMenu fontSize="large" color="action"/>
-                    <h3>Recipe menu project</h3>
-                    <h4>React and Typescript</h4>
-                    <Link to="/disheslist" className="pbtn">Check out</Link>
-                </div>
-                <div className="project">
-                    <RestaurantMenu fontSize="large" color="action"/>
-                    <h3>Recipe menu project</h3>
-                    <h4>React and Typescript</h4>
-                    <Link to="#" className="pbtn">Check out</Link>
-                </div>
-            </section>
+            {/* <div className="skills-container"> */}
+                <section className="skills">
+                    <div className="about">
+                        <h3 className="about-header">My name is Asaf Ovadya</h3>
+                        <p className="about-me">C.S student of The
+                            Open University of Israel.
+                        </p>
+                        <h4>I'm looking for my first job as a Front-end developer.</h4>
+                        <Link to="/contact" className="contact"><button className="contact">Contact me</button></Link>
+                    </div>
+                    <div className="my-image">
+                        {/* <h2 className="skill-header">Skills</h2>
+                        <div className="icons">
+                            <img className="icon" src="https://img.icons8.com/color/144/000000/html-5--v2.png" alt="html"
+                            loading="lazy"/>
+                            <img className="icon" src="https://img.icons8.com/color/144/000000/css3.png" alt="css"
+                            loading="lazy"/>
+                            <img className="icon" src="https://img.icons8.com/color/144/000000/javascript--v1.png" alt="js"
+                            loading="lazy"/>
+                            <img className="icon" src="https://img.icons8.com/color/144/000000/react-native.png" alt="react"
+                            loading="lazy"/>
+                        </div> */}
+                        Future picture here
+                    </div>
+                </section>
+            {/* </div> */}
+            
+            {/* <div className="heightDiv"></div> */}
+            {/* <div className="projects-container"> */}
+                <section className="projects">
+                    <div className="project">
+                        <Casino fontSize="large" color="action"/>
+                        <h3>Mini games project</h3>
+                        <h4>Vanilla Javascript</h4>
+                        <a href="https://small-mini-games-project.netlify.app/" className="pbtn">Check out</a>
+                    </div>
+                    <div className="project">
+                        <RestaurantMenu fontSize="large" color="action"/>
+                        <h3>Recipe menu project</h3>
+                        <h4>React and Typescript</h4>
+                        <Link to="/disheslist" className="pbtn">Check out</Link>
+                    </div>
+                    <div className="project">
+                        <RestaurantMenu fontSize="large" color="action"/>
+                        <h3>Recipe menu project</h3>
+                        <h4>React and Typescript</h4>
+                        <Link to="#" className="pbtn">Check out</Link>
+                    </div>
+                </section>
+            {/* </div> */}
         </main>
     )
 }
